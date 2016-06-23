103373dacc957cf2e763fec19537c5577d7017e4
davidlabragaona <davidlabragaona@gmail.com>
2016-06-23 20:10:49 -0300

    Modificación sobre Ciudad para crear el método que calcula PRIM
    Change-Id: I0000000000000000000000000000000000000001
    Signed-off-by: davidlabragaona <davidlabragaona@gmail.com>

--- Parcial2/src/ar/edu/uno/poo2/parcial/Ciudad.java
+++ Parcial2/src/ar/edu/uno/poo2/parcial/Ciudad.java
@@ -1,5 +1,24 @@
 package ar.edu.uno.poo2.parcial;
 
 public class Ciudad {
+	
+	public Ciudad(int [][] matriz) {
+		
+		//Creamos vectores para el grafo terminado
+		int prim[] = new int[matriz.length];
+		//y le asignamos pesos a las claves
+		int key[] = new int[matriz.length];
+		
+		//para saber que vertice está incluído
+		boolean mstSet[] = new boolean[matriz.length];
+		
+		
+		
+		
+		
+		
+		
+	}
+	
 
 }
