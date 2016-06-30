dd2088e6f095928432cb0bbfb78861a0d876e8c1
Maluc8 <maluc8@gmail.com>
2016-06-30 20:03:01 -0300

    Update Camino.java
    Agregado atributo tipo

--- Parcial2/src/ar/edu/uno/poo2/parcial/Camino.java
+++ Parcial2/src/ar/edu/uno/poo2/parcial/Camino.java
@@ -3,10 +3,12 @@ package ar.edu.uno.poo2.parcial;
 public class Camino {
 	private Isla origen;
 	private Isla destino;
+	private int tipo;
 	
-	public Camino(Isla origen, Isla destino) {
+	public Camino(Isla origen, Isla destino, int tipo) {
 		this.setIslaOrigen(origen);
 		this.setIslaDestino(destino);
+		this.setTipo(tipo);
 	}
 	
 	private void setIslaOrigen(Isla origen) {
@@ -15,6 +17,14 @@ public class Camino {
 	
 	private void setIslaDestino(Isla destino) {
 		this.destino = destino;
+	}
+	
+	public int getTipo() {
+		return tipo;
+	}
+	
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
 	}
 	
 	public int compareTo(Object o) {
